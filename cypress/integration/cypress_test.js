ab83bc3d5d8d6b7e2f7dd946ffe2c676238f3797
Valeri <i450168@fhict.nl>
2024-06-04 15:01:29 +0200

    adding end

--- cypress/integration/cypress_test.js
+++ cypress/integration/cypress_test.js
@@ -3,3 +3,22 @@ describe('My First Test', () => {
       expect(true).to.equal(true)
     })
 })
+
+describe('Testing services', () => {
+  it('Goes to services', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('._font_link_wqkx4_5 > [href="/services"]').click();
+    cy.get('img').should('be.visible');
+/* ==== End Cypress Studio ==== */
+  })
+})
+
+
+describe('Testing about', () => {
+  it('Goes to about', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('._font_link_wqkx4_5 > [href="/about"]').click();
+    cy.get(':nth-child(4) > ._title_target_1n7bx_115').should('be.visible');
+
+  })
+})
