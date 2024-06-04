ec1cc038532f9c6eb03ca205ebdbef34892e0a74
Valeri <i450168@fhict.nl>
2024-06-04 15:16:31 +0200

    adding end-to-end test

--- cypress/integration/cypress_test.js
+++ cypress/integration/cypress_test.js
@@ -7,9 +7,8 @@ describe('My First Test', () => {
 describe('Testing services', () => {
   it('Goes to services', () => {
     cy.visit('http://localhost:5173/');
-    cy.get('._font_link_wqkx4_5 > [href="/services"]').click();
+    cy.get('a[href="/services"]').first().click();
     cy.get('img').should('be.visible');
-/* ==== End Cypress Studio ==== */
   })
 })
 
@@ -17,8 +16,8 @@ describe('Testing services', () => {
 describe('Testing about', () => {
   it('Goes to about', () => {
     cy.visit('http://localhost:5173/');
-    cy.get('._font_link_wqkx4_5 > [href="/about"]').click();
-    cy.get(':nth-child(4) > ._title_target_1n7bx_115').should('be.visible');
+    cy.get('a[href="/about"]').first().click();
+    cy.get('img').should('be.visible');
 
   })
 })
