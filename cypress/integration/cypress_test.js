add09f60085973f4132bbb836d356c0af76ef30c
Valeri <i450168@fhict.nl>
2024-06-04 15:50:34 +0200

    adding end-to-end test

--- cypress/integration/cypress_test.js
+++ cypress/integration/cypress_test.js
@@ -6,8 +6,7 @@ describe('My First Test', () => {
 
 describe('Testing services', () => {
   it('Goes to services', () => {
-    cy.visit('http://localhost:5173/');
-    cy.get('a[href="/services"]', { timeout: 10000 }).first().click();
+    cy.visit('http://localhost:5173/services');
     cy.get('img').should('be.visible');
   })
 })
@@ -15,8 +14,7 @@ describe('Testing services', () => {
 
 describe('Testing about', () => {
   it('Goes to about', () => {
-    cy.visit('http://localhost:5173/');
-    cy.get('a[href="/about"]', { timeout: 10000 }).first().click();
+    cy.visit('http://localhost:5173/about');
     cy.get('img').should('be.visible');
 
   })
